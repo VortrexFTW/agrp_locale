a7833eae18d6d80a2cbb24920961db457cbd2836
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-05-05 12:34:21 -0500

    Start working on locale resource

---
+++ server.js
@@ -0,0 +1,11 @@
+"use strict";
+
+// TO-DO
+
+bindEventHandler("OnResourceStart", thisResource, function(event, resource) {
+	exportFunction("getRawLocaleString", getRawLocaleString);
+});
+
+function getRawLocaleString(localeId, stringName) {
+	return "";
+}
