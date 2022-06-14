73b456e72d32f66d3a4cb1ff18557142802a7c0c
Vortrex <3858226+VortrexFTW@users.noreply.github.com>
2022-06-14 09:16:47 -0500

    Add client and server script

--- client.js
+++ client.js
@@ -17,11 +17,18 @@ let locales = {};
 
 // ===========================================================================
 
-bindEventHandler("OnResourceStart", thisResource, function(event, resource) {
-	localeStrings = loadLocaleStrings();
+bindEventHandler("OnResourceReady", thisResource, function (event, resource) {
+	loadLocales();
+	loadLocaleStrings();
 
 	exportFunction("getRawLocaleString", getRawLocaleString);
 	exportFunction("getRawGroupedLocaleString", getRawGroupedLocaleString);
+	exportFunction("getLocaleString", getLocaleString);
+	exportFunction("getGroupedLocaleString", getGroupedLocaleString);
+	exportFunction("getAvailableLocaleOptions", getAvailableLocaleOptions);
+	exportFunction("getLocales", getLocales);
+	exportFunction("getLocaleFromParams", getLocaleFromParams);
+	exportFunction("doesLocaleStringExist", doesLocaleStringExist);
 });
 
 // ===========================================================================
@@ -39,9 +46,9 @@ function getRawGroupedLocaleString(localeId, stringName, index) {
 // ===========================================================================
 
 function loadLocaleStrings() {
-	for(let i in locales) {
+	for (let i in locales) {
 		let localeStringFile = loadTextFile(`locales/${locales[i].localeStringFile}`);
-		if(localeStringFile != null) {
+		if (localeStringFile != null) {
 			let localeId = locales[i].id;
 			localeStrings[localeId] = JSON.parse(localeStringFile);
 		}
@@ -52,9 +59,82 @@ function loadLocaleStrings() {
 
 function loadLocales() {
 	let localesFile = loadTextFile(`locales.json`);
-	if(localesFile != null) {
+	if (localesFile != null) {
 		locales = JSON.parse(localesFile);
 	}
+}
+
+// ===========================================================================
+
+function getLocales() {
+	return locales;
+}
+
+// ===========================================================================
+
+function getLocaleString(localeId, stringName, ...args) {
+	let tempString = getRawLocaleString(localeId, stringName);
+
+	if (tempString == "" || tempString == null || tempString == undefined) {
+		return "";
+	}
+
+	for (let i = 1; i <= args.length; i++) {
+		tempString = tempString.replace(`{${i}}`, args[i - 1]);
+	}
+
+	return tempString;
+}
+
+// ===========================================================================
+
+function doesLocaleStringExist(localeId, stringName) {
+	let tempString = getRawLocaleString(localeId, stringName);
+
+	if (tempString == "" || tempString == null || tempString == undefined) {
+		return "";
+	}
+}
+
+// ===========================================================================
+
+function getGroupedLocaleString(localeId, stringName, index, ...args) {
+	let tempString = getRawGroupedLocaleString(localeId, stringName, index);
+
+	if (tempString == "" || tempString == null || tempString == undefined) {
+		return "";
+	}
+
+	for (let i = 1; i <= args.length; i++) {
+		tempString = tempString.replace(`{${i}}`, args[i - 1]);
+	}
+
+	return tempString;
+}
+
+// ===========================================================================
+
+function getAvailableLocaleOptions() {
+	return locales.filter(locale => locale.requiresUnicode == false);
+}
+
+// ===========================================================================
+
+function getLocaleFromParams() {
+	let locales = getLocales();
+	if (isNaN(params)) {
+		for (let i in locales) {
+			if (toLowerCase(locales[i].isoCode).indexOf(toLowerCase(params)) != -1) {
+				return i;
+			}
+
+			if (toLowerCase(locales[i].englishName).indexOf(toLowerCase(params)) != -1) {
+				return i;
+			}
+		}
+	}
+
+	return -1;
 }
 
 // ===========================================================================
